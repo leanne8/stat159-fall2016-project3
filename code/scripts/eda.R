b5c98ea2f37171114a4cae5f44dfe2edc236022c
Leanne Lee <leannelee812@berkeley.edu>
2016-11-20 14:42:02 -0800

    eda script

---
+++ code/scripts/eda.R
@@ -0,0 +1,5 @@
+#import datasets 
+CS10_11 <- read.csv("data/CSC10_11.csv")
+CS11_12 <- read.csv("data/CSC11_12.csv")
+CS12_13 <- read.csv("data/CSC12_13.csv")
+CS13_14 <- read.csv("data/CSC13_14.csv")
