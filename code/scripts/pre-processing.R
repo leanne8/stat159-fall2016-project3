cbc10c892cff11ad40b63328cf60112d7831154f
Leanne Lee <leannelee812@berkeley.edu>
2016-11-21 18:29:28 -0800

    changed dataset to 12-13

--- code/scripts/pre-processing.R
+++ code/scripts/pre-processing.R
@@ -1,17 +1,19 @@
 ##### Choosing the top schools with best SAT and ACT scores
 
-data13_14 <- read.csv('data/CSC13_14.csv')
-data13_14$SATMT75 <- as.numeric(as.character(data13_14$SATMT75))
-data13_14$ACTMT75 <- as.numeric(as.character(data13_14$ACTMT75))
+data12_13 <- read.csv('data/CSC12_13.csv')
+data12_13$SATMT75 <- as.numeric(as.character(data12_13$SATMT75))
+data12_13$ACTMT75 <- as.numeric(as.character(data12_13$ACTMT75))
 
-topSAT <- data13_14[which(data13_14$SATMT75 > 730 & data13_14$SATMT75 <= 800),]
-topACT1 <- data13_14[which(data13_14$ACTMT75 > 31 & data13_14$ACTMT75 <= 34),]
-topACT2 <- data13_14[which(data13_14$ACTMT75 > 34 & data13_14$ACTMT75 <= 36),]
+topSAT <- data12_13[which(data12_13$SATMT75 > 730 & data12_13$SATMT75 <= 800),]
+topACT1 <- data12_13[which(data12_13$ACTMT75 > 31 & data12_13$ACTMT75 <= 34),]
+topACT2 <- data12_13[which(data12_13$ACTMT75 > 34 & data12_13$ACTMT75 <= 36),]
 topACT <- rbind(topACT1, topACT2)
 
 topSAT$INSTNM <- as.character(topSAT$INSTNM)
 topACT1$INSTNM <- as.character(topACT1$INSTNM)
 combine <- rbind(topSAT,topACT)
-combineNoOverlap <- unique(combine)
+clean <- unique(combine)
+clean$MN_EARN_WNE_INC3_P10 <- as.numeric(as.character(clean$MN_EARN_WNE_INC3_P10))
+clean <- clean[complete.cases(clean$MN_EARN_WNE_INC3_P10), ]
 #The top 85 colleges with top tier SAT and ACT scores. 
-write.csv(combineNoOverlap, file = './data/top_schools.csv')
+write.csv(clean, file = './data/top_schools.csv')
