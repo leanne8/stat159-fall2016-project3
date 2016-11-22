3483af3572eace25e61ccbd9efcaa757e5b428c7
Leanne Lee <leannelee812@berkeley.edu>
2016-11-21 22:55:04 -0800

    taken account SAT/ACT scores for 1std above

--- code/scripts/pre-processing.R
+++ code/scripts/pre-processing.R
@@ -4,16 +4,20 @@ data12_13 <- read.csv('data/CSC12_13.csv')
 data12_13$SATMT75 <- as.numeric(as.character(data12_13$SATMT75))
 data12_13$ACTMT75 <- as.numeric(as.character(data12_13$ACTMT75))
 
-topSAT <- data12_13[which(data12_13$SATMT75 > 730 & data12_13$SATMT75 <= 800),]
-topACT1 <- data12_13[which(data12_13$ACTMT75 > 31 & data12_13$ACTMT75 <= 34),]
-topACT2 <- data12_13[which(data12_13$ACTMT75 > 34 & data12_13$ACTMT75 <= 36),]
-topACT <- rbind(topACT1, topACT2)
-
+topSAT1 <- data12_13[which(data12_13$SATMT75 > 660 & data12_13$SATMT75 <= 730),]
+topSAT2 <- data12_13[which(data12_13$SATMT75 > 730 & data12_13$SATMT75 <= 800),]
+topACT1 <- data12_13[which(data12_13$ACTMT75 > 28 & data12_13$ACTMT75 <= 31),]
+topACT2 <- data12_13[which(data12_13$ACTMT75 > 31 & data12_13$ACTMT75 <= 34),]
+topACT3 <- data12_13[which(data12_13$ACTMT75 > 34 & data12_13$ACTMT75 <= 36),]
+topACT <- rbind(topACT1, topACT2, topACT3)
+topSAT <- rbind(topSAT1, topSAT2)
 topSAT$INSTNM <- as.character(topSAT$INSTNM)
 topACT1$INSTNM <- as.character(topACT1$INSTNM)
 combine <- rbind(topSAT,topACT)
 clean <- unique(combine)
 clean$MN_EARN_WNE_INC3_P10 <- as.numeric(as.character(clean$MN_EARN_WNE_INC3_P10))
 clean <- clean[complete.cases(clean$MN_EARN_WNE_INC3_P10), ]
-#The top 85 colleges with top tier SAT and ACT scores. 
+clean <- clean[complete.cases(clean$ACTMT75), ]
+
+#The top 146 colleges with top tier SAT and ACT scores. 
 write.csv(clean, file = './data/top_schools.csv')
