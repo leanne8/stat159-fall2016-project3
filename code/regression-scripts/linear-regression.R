580df35dc1a8462d55029b859d1317336a59de72
Leanne Lee <leannelee812@berkeley.edu>
2016-11-22 19:00:16 -0800

    added INC2 and INC1 for multiple linear regressions

--- code/regression-scripts/linear-regression.R
+++ code/regression-scripts/linear-regression.R
@@ -1,33 +1,45 @@
 topSchools <- read.csv('data/top_schools.csv')
 
 cleanData <- topSchools[c("SATMT75", "ACTMT75", "ADM_RATE", "COMP_ORIG_YR4_RT","MN_EARN_WNE_INC3_P10")]
+cleanData2 <- topSchools[c("SATMT75", "ACTMT75", "ADM_RATE", "COMP_ORIG_YR4_RT","MN_EARN_WNE_INC2_P10")]
+cleanData3 <- topSchools[c("SATMT75", "ACTMT75", "ADM_RATE", "COMP_ORIG_YR4_RT","MN_EARN_WNE_INC1_P10")]
 
-mulLM <- lm(MN_EARN_WNE_INC3_P10 ~ ., data = cleanData)
-mulLM
+TopLM <- lm(MN_EARN_WNE_INC3_P10 ~ ., data = cleanData)
+MidLM <- lm(MN_EARN_WNE_INC2_P10 ~ ., data = cleanData2)
+LowLM <- lm(MN_EARN_WNE_INC1_P10 ~ ., data = cleanData3)
 
-LMcorr <- cor(cleanData)
-LMcorr
+TopCorr <- cor(cleanData)
+MidCorr <- cor(cleanData2)
+LowCorr <- cor(cleanData3)
+
 #Find the summary of multiple linear regression
-mulLMSum <- summary(mulLM)
-mulLMSum
+TopLMsum <- summary(TopLM)
+MidLMsum <- summary(MidLM)
+LowLMsum <- summary(LowLM)
 
 #Coeffiecients 
-mulLMCoef <- coef(mulLM)
-mulLMCoef
+TopCoef <- coef(TopLM)
+MidCoef <- coef(MidLM)
+LowCoef <- coef(LowLM)
 
 #MSE for multiple linear regression 
-mulLMMSE <- mean(mulLM$residuals^2)
+TopMSE <- mean(TopLM$residuals^2)
+MidMSE <- mean(MidLM$residuals^2)
+LowMSE <- mean(LowLM$residuals^2)
 
 #save into OLS binary file
-save(mulLM, mulLMSum, mulLMCoef, mulLMMSE, LMcorr, file="./data/multiple-linear-reg.RData")
+save(TopLM, MidLM, LowLM, TopCorr, MidCorr, LowCorr,
+     TopLMsum, MidLMsum, LowLMsum, TopCoef, MidCoef,
+     LowCoef, TopMSE, MidMSE, LowMSE,
+     file="./data/multiple-linear-reg.RData")
 
 sink('./data/multiple-linear-reg-output.txt')
 cat('Muliple Linear Regression summary statistics\n')
-print(mulLMSum)
+print(TopLM)
 cat('Muliple Linear Regression coefficients\n')
-print(mulLMCoef)
+print(TopCoef)
 cat('Muliple Linear Regression MSE\n')
-print(mulLMMSE)
+print(TopMSE)
 cat("Multiple Linear Regression correlation \n")
-print(LMcorr)
+print(TopCorr)
 sink()
