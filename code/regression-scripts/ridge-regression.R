707905cc3bf0cddbfd3781c8927f543535994e2a
Jamie <jamie.stankiewiz@berkeley.edu>
2016-11-22 18:25:33 -0800

    subsetted data set to make train test and full subsets

--- code/regression-scripts/ridge-regression.R
+++ code/regression-scripts/ridge-regression.R
@@ -16,3 +16,30 @@ topSchools_subset1$MN_EARN_WNE_IC3_P10 <- as.numeric(as.character(topSchools_sub
 
 #Mean Centering and Standardizing (scaling the data)
 topSchools_scaled <- scale(topSchools_subset1, center=TRUE, scale=TRUE)
+
+#Partitioning the data
+set.seed(1)
+#nrow(topSchools_scaled)
+
+trainset <- read.csv('data/train-set.csv')
+trainset_subset <- trainset[c('SATMT75', 'ACTMT75', 'ADM_RATE', 'COMP_ORIG_YR4_RT', 'MN_EARN_WNE_INC3_P10')]
+trainset_scaled <- scale(trainset_subset, center=T, scale=T)
+
+testset <- read.csv('data/test-set.csv')
+testset_subset <- trainset[c('SATMT75', 'ACTMT75', 'ADM_RATE', 'COMP_ORIG_YR4_RT', 'MN_EARN_WNE_INC3_P10')]
+testset_scaled <- scale(testset_subset, center=T, scale=T)
+
+x_train_scaled <- as.matrix(trainset_scaled[,1:4])
+y_train_scaled <- as.matrix(trainset_scaled[,5])
+
+x_test_scaled <- as.matrix(testset_scaled[,1:4])
+y_test_scaled <- as.matrix(testset_scaled[,5])
+
+x_full <- as.matrix(topSchools_scaled[,1:4])
+y_full <- as.matrix(topSchools_scaled[,5])
+
+
+#1 Run the corresponding fitting function on the train set using ten-fold cross-validation.
+grid <- 10^seq(10, -2, length = 100)
+set.seed(0)
+cv.out <- cv.glmnet(x_train, y_train, alpha=0, lambda=grid, intercept=FALSE, standardize=FALSE)
