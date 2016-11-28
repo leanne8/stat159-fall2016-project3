8ebb5aaca38af8f8f67d8ef5316a566aac4b68ad
Leanne Lee <leannelee812@berkeley.edu>
2016-11-28 12:29:26 -0800

    fixed the display bug on shiny app

--- code/shiny-app/app.R
+++ code/shiny-app/app.R
@@ -41,8 +41,6 @@ server <- function(input, output) {
   output$picture <- renderPlot({
     if(input$reg == "Ridge Regression" && input$tier == "Top tier") {
       plot(cv.out, main = "Top tier earning in Ridge Regression Cross Validation")
-      coeff_ridge
-      full_mse_ridge
     }
     if(input$reg == "Ridge Regression" && input$tier == "Middle tier") {
       plot(cv.out2, main = "Middle tier earning in Ridge Regression Cross Validation")
@@ -72,16 +70,64 @@ server <- function(input, output) {
     if(input$reg == "Ridge Regression" && input$tier == "Top tier") {
       full_mse_ridge
     }
-    
+    else if(input$reg == "Ridge Regression" && input$tier == "Middle tier") {
+      full_mse_ridge2
+    }
+    else if(input$reg == "Ridge Regression" && input$tier == "Low tier") {
+      full_mse_ridge1
+    }
+    else if(input$reg == "Multiple Linear Regression" && input$tier == "Top tier") {
+      TopMSE 
+    }
+    else if(input$reg == "Multiple Linear Regression" && input$tier == "Middle tier") {
+      MidMSE
+    }
+    else if(input$reg == "Multiple Linear Regression" && input$tier == "Low tier") {
+      LowMSE
+    }
+    else if(input$reg == "Random Forest" && input$tier == "Top tier") {
+      imp
+    }
   })
   output$cor <- renderPrint({
     if(input$reg == "Ridge Regression" && input$tier == "Top tier") {
       ridgecor
     }
+    else if(input$reg == "Ridge Regression" && input$tier == "Middle tier") {
+      ridge2cor
+    }
+    else if(input$reg == "Ridge Regression" && input$tier == "Low tier") {
+      ridge1cor
+    }
+    else if(input$reg == "Multiple Linear Regression" && input$tier == "Top tier") {
+      TopCorr 
+    }
+    else if(input$reg == "Multiple Linear Regression" && input$tier == "Middle tier") {
+      MidCorr
+    }
+    else if(input$reg == "Multiple Linear Regression" && input$tier == "Low tier") {
+      LowCorr
+    }
+    
   })
   output$coeff <- renderPrint({
     if(input$reg == "Ridge Regression" && input$tier == "Top tier") {
       coeff_ridge
+    }
+    else if(input$reg == "Ridge Regression" && input$tier == "Middle tier") {
+      coeff_ridge2
+    }
+    else if(input$reg == "Ridge Regression" && input$tier == "Low tier") {
+      coeff_ridge1
+    }
+    else if(input$reg == "Multiple Linear Regression" && input$tier == "Top tier") {
+      TopCoef 
+    }
+    else if(input$reg == "Multiple Linear Regression" && input$tier == "Middle tier") {
+      MidCoef
+    }
+    else if(input$reg == "Multiple Linear Regression" && input$tier == "Low tier") {
+      LowCoef
     }
   })
 }
