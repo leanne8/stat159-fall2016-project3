fc520fab1a4c7347166eea7ba4a3a600dbf3fe88
Jamie <jamie.stankiewiz@berkeley.edu>
2016-11-29 20:53:40 -0800

    added PELL_COMP_ORIG_YR6_RT to all ridge regressions used from the new data and new images were produced

--- code/regression-scripts/ridge-regression-1.R
+++ code/regression-scripts/ridge-regression-1.R
@@ -4,7 +4,7 @@ topSchools <- read.csv('data/top_schools.csv')
 #View(topSchools)
 
 #Subset the only columns I need to use for ridge regression
-topSchools_subset <- topSchools[c('SATMT75', 'ACTMT75', 'ADM_RATE', 'COMP_ORIG_YR4_RT', 'MN_EARN_WNE_INC1_P10')]
+topSchools_subset <- topSchools[c('SATMT75', 'ACTMT75', 'ADM_RATE', 'COMP_ORIG_YR4_RT', "PELL_COMP_ORIG_YR6_RT", 'MN_EARN_WNE_INC1_P10')]
 
 topSchools_subset1 <- topSchools_subset
 #transforming each column type so it will be numeric and will work when I try to scale the data set.
@@ -12,6 +12,7 @@ topSchools_subset1$SATMT75 <- as.numeric(as.character(topSchools_subset$SATMT75)
 topSchools_subset1$ACTMT75 <- as.numeric(as.character(topSchools_subset$ACTMT75))
 topSchools_subset1$ADM_RATE <- as.numeric(as.character(topSchools_subset$ADM_RATE))
 topSchools_subset1$COMP_ORIG_YR4_RT <- as.numeric(as.character(topSchools_subset$COMP_ORIG_YR4_RT))
+topSchools_subset1$PELL_COMP_ORIG_YR6_RT <- as.numeric(as.character(topSchools_subset$PELL_COMP_ORIG_YR6_RT))
 topSchools_subset1$MN_EARN_WNE_IC1_P10 <- as.numeric(as.character(topSchools_subset$MN_EARN_WNE_INC1_P10))
 
 #Mean Centering and Standardizing (scaling the data)
@@ -22,28 +23,28 @@ set.seed(12345)
 #nrow(topSchools_scaled)
 
 trainset <- read.csv('data/train-set.csv')
-trainset_subset <- trainset[c('SATMT75', 'ACTMT75', 'ADM_RATE', 'COMP_ORIG_YR4_RT', 'MN_EARN_WNE_INC1_P10')]
+trainset_subset <- trainset[c('SATMT75', 'ACTMT75', 'ADM_RATE', 'COMP_ORIG_YR4_RT', 'PELL_COMP_ORIG_YR6_RT','MN_EARN_WNE_INC1_P10')]
 trainset_subset$MN_EARN_WNE_INC1_P10 <- as.numeric(as.character(trainset_subset$MN_EARN_WNE_INC1_P10))
 trainset_scaled <- scale(trainset_subset, center=T, scale=T)
 
 trainset_scaled <- trainset_scaled[complete.cases(trainset_scaled),]
 
 testset <- read.csv('data/test-set.csv')
-testset_subset <- trainset[c('SATMT75', 'ACTMT75', 'ADM_RATE', 'COMP_ORIG_YR4_RT', 'MN_EARN_WNE_INC1_P10')]
+testset_subset <- trainset[c('SATMT75', 'ACTMT75', 'ADM_RATE', 'COMP_ORIG_YR4_RT','PELL_COMP_ORIG_YR6_RT','MN_EARN_WNE_INC1_P10')]
 testset_subset$MN_EARN_WNE_INC1_P10 <- as.numeric(as.character(testset_subset$MN_EARN_WNE_INC1_P10))
 testset_scaled <- scale(testset_subset, center=T, scale=T)
 
 testset_scaled <- testset_scaled[complete.cases(testset_scaled),]
 
-x_train_scaled <- as.matrix(trainset_scaled[,1:4])
-y_train_scaled <- as.matrix(trainset_scaled[,5])
+x_train_scaled <- as.matrix(trainset_scaled[,1:5])
+y_train_scaled <- as.matrix(trainset_scaled[,6])
 
 
-x_test_scaled <- as.matrix(testset_scaled[,1:4])
-y_test_scaled <- as.matrix(testset_scaled[,5])
+x_test_scaled <- as.matrix(testset_scaled[,1:5])
+y_test_scaled <- as.matrix(testset_scaled[,6])
 
-x_full <- as.matrix(topSchools_scaled[,1:4])
-y_full <- as.matrix(topSchools_scaled[,5])
+x_full <- as.matrix(topSchools_scaled[,1:5])
+y_full <- as.matrix(topSchools_scaled[,6])
 
 
 #1 Run the corresponding fitting function on the train set using ten-fold cross-validation.
