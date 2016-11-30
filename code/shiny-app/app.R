2545965730ecb20dfbebae2080f91850251bb89b
Leanne Lee <leannelee812@berkeley.edu>
2016-11-29 23:53:44 -0800

    delete correlation and add comparsin plots

--- code/shiny-app/app.R
+++ code/shiny-app/app.R
@@ -22,8 +22,8 @@ ui <- fluidPage(
       tabsetPanel(
                   tabPanel("Plot", plotOutput("picture")), 
                   tabPanel("MSE", verbatimTextOutput("mse")), 
-                  tabPanel("Correlation", verbatimTextOutput("cor")),
-                  tabPanel("Coefficients", verbatimTextOutput("coeff"))
+                  tabPanel("Coefficients", verbatimTextOutput("coeff")),
+                  tabPanel("Comparison", plotOutput("compare"))
       )
     )
   
@@ -98,28 +98,6 @@ server <- function(input, output) {
     }
   })
 
-  output$cor <- renderPrint({
-    if(input$reg == "Ridge Regression" && input$tier == "Top tier") {
-      ridgecor
-    }
-    else if(input$reg == "Ridge Regression" && input$tier == "Middle tier") {
-      ridge2cor
-    }
-    else if(input$reg == "Ridge Regression" && input$tier == "Low tier") {
-      ridge1cor
-    }
-    else if(input$reg == "Multiple Linear Regression" && input$tier == "Top tier") {
-      TopCorr 
-    }
-    else if(input$reg == "Multiple Linear Regression" && input$tier == "Middle tier") {
-      MidCorr
-    }
-    else if(input$reg == "Multiple Linear Regression" && input$tier == "Low tier") {
-      LowCorr
-    }
-    #Random Forest doesn't have correlation
-  })
-  
   output$coeff <- renderPrint({
     if(input$reg == "Ridge Regression" && input$tier == "Top tier") {
       coeff_ridge
@@ -141,6 +119,33 @@ server <- function(input, output) {
     }
     #Random Forest doesn't have coefficients
   })
+  
+  output$compare <- renderPlot({
+    if(input$tier == "Top tier") {
+      par(mfrow=c(2,4))
+      plot(cv.out, main = "Top tier earning in Ridge Regression Cross Validation")
+      plot(TopLM, main = "Top tier earning in Multiple Linear Regression")
+      plot(rfModel, main = "Top tier earning in Random Forest model")
+      plot(varplot, main = "Variance Importance in Random Forest")
+      
+    }
+    else if(input$tier == "Middle tier") {
+      par(mfrow=c(2,4))
+      plot(cv.out2, main = "Middle tier earning in Ridge Regression Cross Validation")
+      plot(MidLM, main = "Middle tier earning in Multiple Linear Regression")
+      plot(rfModelMid, main = "Middle tier earning in Random Forest model")
+      plot(varplotMid, main = "Variance Importance in Random Forest")
+    }
+    else if(input$tier == "Low tier") {
+      par(mfrow=c(2,4))
+      plot(cv.out1, main = "Low tier earning in Ridge Regression Cross Validation")
+      plot(LowLM, main = "Low tier earning in Multiple Linear Regression")
+      plot(rfModelLow, main = "Low tier earning in Random Forest model")
+      plot(varplotLow, main = "Variance Importance in Random Forest")
+    }
+
+  })
+  
 }
 
 shinyApp(ui = ui, server = server)
