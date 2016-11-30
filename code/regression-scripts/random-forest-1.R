621062e297f5698fa074391588d558637b65cc8b
Leanne Lee <leannelee812@berkeley.edu>
2016-11-29 23:43:45 -0800

    add pell grant variables to random forest

--- code/regression-scripts/random-forest-1.R
+++ code/regression-scripts/random-forest-1.R
@@ -2,14 +2,13 @@
 library(randomForest)
 
 trainSet = read.csv("data/train-set.csv", header = TRUE)
-trainSet = trainSet[c("SATMT75", "ACTMT75", "ADM_RATE", "COMP_ORIG_YR4_RT","MN_EARN_WNE_INC1_P10")]
+trainSet = trainSet[c("SATMT75", "ACTMT75", "ADM_RATE", "COMP_ORIG_YR4_RT","PELL_COMP_ORIG_YR6_RT", "MN_EARN_WNE_INC1_P10")]
 trainSet$MN_EARN_WNE_INC1_P10 <- as.numeric(as.character(trainSet$MN_EARN_WNE_INC1_P10))
 trainSet <- trainSet[complete.cases(trainSet$MN_EARN_WNE_INC1_P10),]
 trainSet = data.frame(scale(trainSet, center=TRUE, scale=TRUE))
 
-
 testSet = read.csv("data/test-set.csv", header = TRUE)
-testSet = testSet[c("SATMT75", "ACTMT75", "ADM_RATE", "COMP_ORIG_YR4_RT","MN_EARN_WNE_INC1_P10")]
+testSet = testSet[c("SATMT75", "ACTMT75", "ADM_RATE", "COMP_ORIG_YR4_RT","PELL_COMP_ORIG_YR6_RT","MN_EARN_WNE_INC1_P10")]
 testSet$MN_EARN_WNE_INC1_P10 <- as.numeric(as.character(testSet$MN_EARN_WNE_INC1_P10))
 testSet <- testSet[complete.cases(testSet$MN_EARN_WNE_INC1_P10),]
 testSet =  data.frame(scale(testSet, center=TRUE, scale=TRUE))
@@ -19,7 +18,7 @@ png("images/rf_low.png")
 plot(rfModelLow)
 dev.off()
 #We plot the error rate across decision trees.
-#The plot shows that the error become more stable after 100 decision trees.
+#The plot shows that the error become more stable after 120 decision trees.
 
 impLow <- importance(rfModelLow,type = 2)
 
--- code/regression-scripts/random-forest.R
+++ code/regression-scripts/random-forest.R
@@ -2,15 +2,17 @@
 library(randomForest)
 
 trainSet = read.csv("data/train-set.csv", header = TRUE)
-trainSet = trainSet[c("SATMT75", "ACTMT75", "ADM_RATE", "COMP_ORIG_YR4_RT","MN_EARN_WNE_INC3_P10")]
+trainSet = trainSet[c("SATMT75", "ACTMT75", "ADM_RATE", "COMP_ORIG_YR4_RT","PELL_COMP_ORIG_YR6_RT", "MN_EARN_WNE_INC3_P10")]
 trainSet = data.frame(scale(trainSet, center=TRUE, scale=TRUE))
+trainSet <- trainSet[complete.cases(trainSet$MN_EARN_WNE_INC3_P10), ]
 testSet = read.csv("data/test-set.csv", header = TRUE)
-testSet = testSet[c("SATMT75", "ACTMT75", "ADM_RATE", "COMP_ORIG_YR4_RT","MN_EARN_WNE_INC3_P10")]
+testSet = testSet[c("SATMT75", "ACTMT75", "ADM_RATE", "COMP_ORIG_YR4_RT","PELL_COMP_ORIG_YR6_RT","MN_EARN_WNE_INC3_P10")]
 testSet =  data.frame(scale(testSet, center=TRUE, scale=TRUE))
+testSet <- testSet[complete.cases(testSet$MN_EARN_WNE_INC3_P10),]
 rfModel = randomForest(MN_EARN_WNE_INC3_P10 ~ ., trainSet, ntree= 500)
 plot(rfModel)
 #We plot the error rate across decision trees.
-#The plot shows that the error become more stable after 100 decision trees.
+#The plot shows that the error become more stable after 50 decision trees.
 
 imp <- importance(rfModel,type = 2)
 
