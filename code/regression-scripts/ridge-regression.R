56b4a8487935f691e05b455619eff001491b2d82
Jamie <jamie.stankiewiz@berkeley.edu>
2016-12-01 00:21:05 -0800

    added ridge_full to the .RData files

--- code/regression-scripts/ridge-regression.R
+++ code/regression-scripts/ridge-regression.R
@@ -77,7 +77,7 @@ full_mse_ridge <- mean((ridge_pred_full-y_full)^2)
 
 ridgecor <- cor(topSchools_subset)
 
-save(cv.out, coeff_ridge, bestlambda, full_mse_ridge, ridgecor, file='data/rigde-reg.RData')
+save(cv.out, coeff_ridge, bestlambda, full_mse_ridge, ridgecor, ridge_full, file='data/rigde-reg.RData')
 
 #Plot the cross-validation errors
 png("images/ridgeCV_top.png")
